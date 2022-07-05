41f3d0673f0e98eecff33c7d7503e5c0a535ef84
Lucas Neves <lzmn70@gmail.com>
2022-07-04 21:26:48 -0300

    Adicionado arquivos base para extensão. Imagens, arquivo html, css inicial e readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# moreTools-extension
+# MoreTools extension
+
+## :us:A speed and easy way to acess website aplications when you need.
+
+This extention was been created to make your activites more easy, just open one of the tabs and choose the web application that better help you in the moment. The base of this project is to make de realization of your project more practical.
+
+## :brazil:Uma maneira rápida e fácil de acessar aplicações de websites quando precisar.
+
+A extensão foi criada para facilitar o seu dia a dia, basta abrir uma das abas e escolher a aplicação web que pode melhor te ajudar naquele momento. A raiz desse projeto é tornar a realização do seu dever mais prática.
---
+++ tools.js
@@ -0,0 +1,95 @@
+    var creately = {
+        nome: "Creately",
+        url: "https://creately.com/",
+        icon: "img/websites/daily/creately.png"
+    }
+
+    var cip = {
+        nome: "Color Image Picker",
+        url: "https://imagecolorpicker.com/",
+        icon: "img/websites/daily/CIP.png"
+    }
+
+    var trello = {
+        nome: "Trello",
+        url: "trello.com",
+        icon: "img/websites/daily/trello.png"
+    }
+
+    var iLovePdf = {
+        nome: "iLovePDF",
+        url: "https://www.ilovepdf.com/",
+        icon: "img/websites/daily/iLovePDF.png"
+    }
+
+var daily = [creately, cip, trello, iLovePdf];
+
+    var simplenote = {
+        nome:"Simplenote",
+        url:"simplenote.com",   
+        icon:"img/websites/anotacoes/simplenote.png"
+}
+
+    var evernote = {
+        nome:"Evernote",
+        url:"evernote.com",
+        icon:"img/websites/anotacoes/evernote.png"
+}
+
+    var keep = {
+        nome:"Google Keep",
+        url:"keep.google.com",
+        icon:"img/websites/anotacoes/keep.png"
+}
+
+var anotacoes = [simplenote, evernote, keep];
+
+    var onedrive = {
+        nome: "Onedrive",
+        url:"https://onedrive.live.com/",
+        icon:"img/websites/cloud/onedrive.png"
+    }
+
+    var mega = {
+        nome: "MegaSync",
+        url:"mega.io",
+        icon: "img/websites/cloud/mega.ico"
+    }
+
+    var drive = {
+        nome: "Google Drive",
+        url: "drive.google.com",
+        icon: "img/websites/cloud/drive.png"
+    }
+    
+    var dropbox = {
+        nome: "Dropbox",
+        url: "https://www.dropbox.com/",
+        icon: "img/websites/cloud/dropbox.png"
+    }
+
+var cloud = [onedrive, mega, drive, dropbox];
+
+    var amazon = {
+        nome: "Amazon",
+        url: "www.amazon.com",
+        icon: "img/websites/shopping/amazon.png"    
+    }
+
+    var mercadoLivre = {
+        nome: "Mercado Livre",
+        url: "https://www.mercadolivre.com.br/",
+        icon: "img/websites/shopping/mercadoLivre.svg"
+    }
+
+    var shopee = { 
+        nome: "Shopee",
+        url: "https://shopee.com.br/",
+        icon: "img/websites/shopping/shopee.png"
+    }
+
+var shopping = [amazon, mercadoLivre, shopee];
+
+var abas = [daily, anotacoes, cloud, shopping];
+console.log(abas);
+console.log("ola");
